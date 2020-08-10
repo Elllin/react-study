23d04bd8f833d2de10de60e5b5fbc8e840dad6ee
Elllin <noto01@naver.com>
2020-08-10 21:09:21 +0900

    Feat : 메인페이지 router 추가
    존재하지 않는 페이지 예외처리

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import HomeContainer from './containers/HomeContainer';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/" component={HomeContainer} exact />
+        <Route
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다. :</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function HomeContainer() {
-  return <></>;
+  return <>home</>;
 }
 
 export default HomeContainer;
