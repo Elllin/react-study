02fc69b67b794285824e86031301371aaee5d480
Elllin <noto01@naver.com>
2020-08-10 22:04:50 +0900

    Feat : header 컴포넌트 렌더링, background color 추가

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Header() {
-  return <></>;
+  return <HeaderBar></HeaderBar>;
 }
+
+const HeaderBar = styled.header`
+  width: 100%;
+  height: 50px;
+  background-color: pink;
+`;
 
 export default Header;
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Header from '../components/Home/Header/Header';
 
 function HomeContainer() {
-  return <>home</>;
+  return <Header>home</Header>;
 }
 
 export default HomeContainer;
