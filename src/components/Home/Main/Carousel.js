a7c4f94a56ed277739a57533c288d8e9d2f583af
Elllin <noto01@naver.com>
2020-08-12 11:57:44 +0900

    Feat : carousel arrow 버튼 커스텀
    < > 화살표 react icon으로 변경

--- src/components/Home/Main/Carousel.js
+++ src/components/Home/Main/Carousel.js
@@ -2,20 +2,24 @@ import React from 'react';
 import Slider from 'react-slick';
 
 import styled from 'styled-components';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 function Carousel() {
   const carouselSettins = {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
+    infinite: true,
     autoplaySpeed: 4000,
-    arrows: true,
+    // arrows: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
   };
   return (
     <SliderWrap>
       <Slider {...carouselSettins}>
         <div>
-          <h3>1</h3>
+          <h3>1{/* <FaChevronRight /> */}</h3>
         </div>
         <div>
           <h3>2</h3>
@@ -29,6 +33,7 @@ function Carousel() {
 }
 
 const SliderWrap = styled.div`
+  position: relative;
   width: 100%;
   height: 300px;
   margin: 0 auto;
@@ -39,6 +44,32 @@ const SliderWrap = styled.div`
   h3 {
     color: white;
     font-size: 300px;
+  }
+`;
+
+const PrevArrow = styled(FaChevronLeft)`
+  position: absolute;
+  font-size: 50px;
+  left: 0;
+  z-index: 1;
+  height: 30px;
+  width: 30px;
+  color: #fff;
+  &:hover,
+  &:focus {
+    color: #fff;
+  }
+`;
+
+const NextArrow = styled(FaChevronRight)`
+  color: white;
+  position: absolute;
+  right: 0;
+  height: 30px;
+  width: 30px;
+  &:hover,
+  &:focus {
+    color: #fff;
   }
 `;
 
