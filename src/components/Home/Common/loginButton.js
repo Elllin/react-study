9522180c8873b4b4e8230390d65b144d79f0ce44
Elllin <noto01@naver.com>
2020-08-15 11:38:27 +0900

    Feat : 로그인 버튼 href 추가

--- src/components/Home/Common/loginButton.js
+++ src/components/Home/Common/loginButton.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 function LoginButton() {
   return (
-    <button>
+    <a href="https://slack.com/oauth/authorize?user_scope=incoming-webhook&client_id=1279458981345.1293918756150">
       <img
         src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
         alt="슬랙으로 로그인"
         srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"
       />
-    </button>
+    </a>
   );
 }
 
