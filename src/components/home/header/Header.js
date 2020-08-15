14da860f80623951117da2df5bc071a1fdb4e8c2
Elllin <noto01@naver.com>
2020-08-16 01:18:02 +0900

    rename : 폴더명 대문자 -> 소문자 변경

--- src/components/home/header/Header.js
+++ src/components/home/header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import LoginButton from '../Common/LoginButton';
+import LoginButton from '../common/LoginButton';
 
 import styled from 'styled-components';
 import { Container, Layout } from 'style/CustomStyle';
