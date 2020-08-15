4ea14e78c96ff0d4e1dfec0187086144d0eba06e
Elllin <noto01@naver.com>
2020-08-16 01:07:54 +0900

    Refactor : 폴더명 소문자 -> 대문자 변경

---
+++ src/components/home/Common/loginButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LoginButton() {
+  return (
+    <img
+      src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
+      alt="슬랙으로 로그인"
+      // srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"
+    />
+  );
+}
+
+export default LoginButton;
--- src/components/home/Main/description/Description.js
+++ src/components/home/Main/description/Description.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import styled from 'styled-components';
-import { Container, Layout } from 'style/CustomStyle';
+import { Container } from 'style/CustomStyle';
 
 function Description() {
   return (
