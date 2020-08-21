a1cda43426309f51ad43efbbee2bbd1c60b704d0
Elllin <noto01@naver.com>
2020-08-21 21:53:59 +0900

    Refactor : 스터디 개설 폼 위치 수정

--- src/components/studyOpening/StudyOpening.js
+++ src/components/studyOpening/StudyOpening.js
@@ -8,19 +8,35 @@ function StudyOpening() {
   const { register, handleSubmit, errors } = useForm();
 
   const onSubmit = (data) => {
-    console.log(data);
+    const formData = JSON.stringify(data);
+    console.log(formData);
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: formData,
+    }).then((data) => console.log(data));
+    //try catch를 통해 tru라면? 화면 이동하기
   };
 
   return (
     <StudyForm action="#" method="post" onSubmit={handleSubmit(onSubmit)}>
       <fieldset>
         <div>
-          <legend>스터디 개설하기</legend>
+          <legend>
+            DO IT DO IT
+            <br /> 스터디 그룹 개설하기
+          </legend>
           <BoxWrap>
             <Title htmlFor="title">
-              스터디 그룹 이름 <Required>*</Required>
+              그룹 이름 <Required>*</Required>
             </Title>
+
             <Input type="text" id="title" name="title" ref={register} />
+          </BoxWrap>
+          <BoxWrap>
+            <Title htmlFor="description">
+              그룹 소개 <Required>*</Required>
+            </Title>
+            <TextArea as="textarea" id="description" name="description" ref={register}></TextArea>
           </BoxWrap>
           <BoxWrap>
             <Title htmlFor="email">
@@ -33,61 +49,43 @@ function StudyOpening() {
               <Title htmlFor="location">
                 지역 <Required>*</Required>
               </Title>
-              <Input type="text" id="location" name="location" ref={register} />
+              <Input as="select" id="location" name="location" ref={register}>
+                <option value="서울">서울</option>
+                <option value="경기">경기</option>
+                <option value="인천">인천</option>
+              </Input>
             </BoxWrap>
             <BoxWrap>
-              <Title htmlFor="time">
-                시간 <Required>*</Required>
+              <Title htmlFor="category">
+                그룹 카테고리 <Required>*</Required>
               </Title>
-              <Input type="text" id="time" name="time" ref={register} />
+              <Input as="select" id="category" name="category" ref={register}>
+                <option value="대입 / 수능">대입 / 수능</option>
+                <option value="대학생 / 취업">대학생 / 취업</option>
+                <option value="공무원 / 임용">공무원 / 임용</option>
+              </Input>
             </BoxWrap>
           </BoxLayout>
 
           <BoxWrap>
             <Title as="div">
-              예치금 여부 <Required>*</Required>
+              예치금 설정을 하시나요? <Required>*</Required>
             </Title>
-            <label htmlFor="yes">예</label>
+            <label htmlFor="yes">네. 할래요!</label>
             <input type="radio" id="yes" name="is_deposit" value="예" ref={register} />
-            <label htmlFor="no">아니오</label>
+            <label htmlFor="no">아니요. 괜찮아요!</label>
             <input type="radio" id="no" name="is_deposit" value="아니오" ref={register} />
           </BoxWrap>
           <BoxWrap>
-            <Title htmlFor="category">
-              카테고리 <Required>*</Required>
-            </Title>
-            <Input as="select" id="category" name="category" ref={register}>
-              <option value="대입 / 수능">대입 / 수능</option>
-              <option value="대학생 / 취업">대학생 / 취업</option>
-              <option value="공무원 / 임용">공무원 / 임용</option>
-            </Input>
-          </BoxWrap>
-          <BoxWrap>
-            <Title htmlFor="description">
-              스터디 설명 <Required>*</Required>
-            </Title>
-            <TextArea as="textarea" id="description" name="description" ref={register}></TextArea>
+            <Title htmlFor="hashtag">그룹 해시태그</Title>
+            <Input type="text" id="hashtag" name="hashtag" />
           </BoxWrap>
           {/* <BoxWrap>
-            <Title htmlFor="title">해시태그</Title>
-            <Input type="text" id="title" name="title" />
-          </BoxWrap>
-          <BoxLayout>
-            <BoxWrap>
-              <Title htmlFor="title">기간</Title>
-              <Input type="text" id="title" name="title" />
-            </BoxWrap>
-            <BoxWrap>
-              <Title htmlFor="title">희망 인원</Title>
-              <Input type="text" id="title" name="title" />
-            </BoxWrap>
-          </BoxLayout>*/}
-          <BoxWrap>
             <Title htmlFor="image">썸네일</Title>
             <Input type="file" id="image" name="image" ref={register} />
-          </BoxWrap>
+          </BoxWrap> */}
         </div>
-        <button type="submit">제출하기</button>
+        <button type="submit">승인요청</button>
       </fieldset>
     </StudyForm>
   );
@@ -115,7 +113,7 @@ const StudyForm = styled.form`
   }
 `;
 
-const Title = styled.p`
+const Title = styled.label`
   margin-right: 30px;
   display: block;
   width: 100%;
