4605dee04d1175eb0af64b414cd6764798ad3b1f
Elllin <noto01@naver.com>
2020-08-21 22:27:27 +0900

    Fix : applyMiddleware를 import안해서 생긴 오류 수정

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
 import ReduxThunk from 'redux-thunk';
