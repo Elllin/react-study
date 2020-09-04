a226850c3d982f641c85644eeff2b03a60962283
Elllin <noto01@naver.com>
2020-09-04 23:27:32 +0900

    Refactor : 스터디 form wrap 컴포넌트 분리

--- src/components/createStudy/CreateStudy.js
+++ src/components/createStudy/CreateStudy.js
@@ -8,8 +8,9 @@ import BoxWrap from './boxWrap/BoxWrap';
 import SelectBox from './selectBox/SelectBox';
 import RadioBox from './radioBox/RadioBox';
 
-import styled, { css } from 'styled-components';
-import { Layout, InputBox } from 'style/CustomStyle';
+import styled from 'styled-components';
+import { InputBox } from 'style/CustomStyle';
+import FormWrap from './FormWrap';
 
 function CreateStudy({ onSubmit }) {
   const { register, handleSubmit, errors } = useForm();
@@ -66,123 +67,75 @@ function CreateStudy({ onSubmit }) {
     hashtag: { placeholder, description },
   } = createStudy;
   return (
-    <Wrap>
-      <h1>DO IT DO IT</h1>
-      <StudyForm action="#" method="post" onSubmit={handleSubmit(onSubmit)}>
-        <fieldset>
-          <div>
-            <legend>스터디 그룹 개설하기</legend>
-            <RequiredMessage>* 는 필수 입력 사항입니다.</RequiredMessage>
-            <BoxLayout>
-              <BoxWrap title="지역" htmlFor="location">
-                <SelectBox optionItems={locationOption} name="location" ref={register} required />
-              </BoxWrap>
-              <BoxWrap title="그룹 카테고리" htmlFor="category">
-                <SelectBox optionItems={categoryOption} name="category" ref={register} required />
-              </BoxWrap>
-            </BoxLayout>
-            <BoxWrap title="예치금 설정을 하시나요?" as="div" far>
-              <RadioBox id="yes" text="네. 할래요!" name="is_deposit" ref={register} required />
-              <RadioBox
-                id="no"
-                text="아니요. 괜찮아요!"
-                name="is_deposit"
-                ref={register}
-                required
-              />
-            </BoxWrap>
-            <BoxWrap title="그룹 이름" htmlFor="title">
-              <InputBox
-                type="text"
-                id="title"
-                name="title"
-                ref={register}
-                required
-                placeholder={groupName}
-              />
-            </BoxWrap>
-            <BoxWrap title="그룹 소개" htmlFor="description">
-              <TextArea
-                as="textarea"
-                id="description"
-                name="description"
-                ref={register}
-                required
-                placeholder={groupIntroduction}
-              ></TextArea>
-            </BoxWrap>
-            {/* <BoxWrap>
+    <FormWrap onSubmit={onSubmit} handleSubmit={handleSubmit}>
+      <BoxLayout>
+        <BoxWrap title="지역" htmlFor="location">
+          <SelectBox optionItems={locationOption} name="location" ref={register} required />
+        </BoxWrap>
+        <BoxWrap title="그룹 카테고리" htmlFor="category">
+          <SelectBox optionItems={categoryOption} name="category" ref={register} required />
+        </BoxWrap>
+      </BoxLayout>
+      <BoxWrap title="예치금 설정을 하시나요?" as="div" far>
+        <RadioBox id="yes" text="네. 할래요!" name="is_deposit" ref={register} required />
+        <RadioBox id="no" text="아니요. 괜찮아요!" name="is_deposit" ref={register} required />
+      </BoxWrap>
+      <BoxWrap title="그룹 이름" htmlFor="title">
+        <InputBox
+          type="text"
+          id="title"
+          name="title"
+          ref={register}
+          required
+          placeholder={groupName}
+        />
+      </BoxWrap>
+      <BoxWrap title="그룹 소개" htmlFor="description">
+        <TextArea
+          as="textarea"
+          id="description"
+          name="description"
+          ref={register}
+          required
+          placeholder={groupIntroduction}
+        ></TextArea>
+      </BoxWrap>
+      {/* <BoxWrap>
               <Title htmlFor="email">
                 이메일 <Required>*</Required>
               </Title>
               <InputBox type="email" id="email" name="email" ref={register} required />
             </BoxWrap> */}
 
-            <BoxWrap title="그룹 해시태그" htmlFor="hashtag">
-              <InputBox
-                type="text"
-                id="hashtag"
-                name="hashtag"
-                required
-                placeholder={placeholder}
-                value={hashtag}
-                onChange={onChangeHashtag}
-              />
-              <Description>{description}</Description>
-              <TagContainer>
-                {hashtags.map((tag) => (
-                  <Hashtag key={tag.id} tag={tag} onClick={removeHashtag} />
-                ))}
-              </TagContainer>
-            </BoxWrap>
+      <BoxWrap title="그룹 해시태그" htmlFor="hashtag">
+        <InputBox
+          type="text"
+          id="hashtag"
+          name="hashtag"
+          required
+          placeholder={placeholder}
+          value={hashtag}
+          onChange={onChangeHashtag}
+        />
+        <Description>{description}</Description>
+        <TagContainer>
+          {hashtags.map((tag) => (
+            <Hashtag key={tag.id} tag={tag} onClick={removeHashtag} />
+          ))}
+        </TagContainer>
+      </BoxWrap>
 
-            {/* <BoxWrap>
+      {/* <BoxWrap>
             <Title htmlFor="image">썸네일</Title>
             <InputBox type="file" id="image" name="image" ref={register} />
           </BoxWrap> */}
 
-            <ButtonWrap>
-              <button type="submit">그룹 개설하기</button>
-            </ButtonWrap>
-          </div>
-        </fieldset>
-      </StudyForm>
-    </Wrap>
+      <ButtonWrap>
+        <button type="submit">그룹 개설하기</button>
+      </ButtonWrap>
+    </FormWrap>
   );
 }
-
-const Wrap = styled.main`
-  margin: 9rem auto;
-  width: 57rem;
-  font-family: ${({ theme }) => theme.formFont};
-  h1 {
-    margin-bottom: 1.3rem;
-    text-align: center;
-    font-size: 2.4rem;
-    font-weight: 900;
-  }
-`;
-
-const StudyForm = styled.form`
-  legend {
-    margin-bottom: 4.4rem;
-    font-family: ${({ theme }) => theme.subFont};
-    font-size: 3.2rem;
-    letter-spacing: -0.06rem;
-    text-align: center;
-  }
-  fieldset > div {
-    ${Layout}
-    flex-direction: column;
-    align-items: normal;
-  }
-`;
-
-const RequiredMessage = styled.span`
-  margin: 0 0 3rem 0.3rem;
-  font-size: 1.6rem;
-  color: ${({ theme }) => theme.requiredColor};
-`;
 
 const TextArea = styled(InputBox)`
   width: 100%;
---
+++ src/components/createStudy/FormWrap.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import styled from 'styled-components';
+import { Layout } from 'style/CustomStyle';
+
+function FormWrap({ children, handleSubmit, onSubmit }) {
+  return (
+    <Wrap>
+      <h1>DO IT DO IT</h1>
+      <StudyForm action="#" method="post" onSubmit={handleSubmit(onSubmit)}>
+        <fieldset>
+          <div>
+            <legend>스터디 그룹 개설하기</legend>
+            <RequiredMessage>* 는 필수 입력 사항입니다.</RequiredMessage>
+            {children}
+          </div>
+        </fieldset>
+      </StudyForm>
+    </Wrap>
+  );
+}
+
+const Wrap = styled.main`
+  margin: 9rem auto;
+  width: 57rem;
+  font-family: ${({ theme }) => theme.formFont};
+  h1 {
+    margin-bottom: 1.3rem;
+    text-align: center;
+    font-size: 2.4rem;
+    font-weight: 900;
+  }
+`;
+
+const StudyForm = styled.form`
+  legend {
+    margin-bottom: 4.4rem;
+    font-family: ${({ theme }) => theme.subFont};
+    font-size: 3.2rem;
+    letter-spacing: -0.06rem;
+    text-align: center;
+  }
+  fieldset > div {
+    ${Layout}
+    flex-direction: column;
+    align-items: normal;
+  }
+`;
+
+const RequiredMessage = styled.span`
+  margin: 0 0 3rem 0.3rem;
+  font-size: 1.6rem;
+  color: ${({ theme }) => theme.requiredColor};
+`;
+
+export default FormWrap;
