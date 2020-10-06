79db6727bb818b182436932b7c82a02df12088e6
Elllin <noto01@naver.com>
2020-10-06 23:15:51 +0900

    Feat : 스터디 리스트 컨테이너
    data, loading, error 불러와 렌더링

--- src/containers/StudyListContainer.js
+++ src/containers/StudyListContainer.js
@@ -6,12 +6,16 @@ import { toggleSelect, closeSelect, fetchStudyList } from 'modules/studyList';
 import Header from 'components/common/header/Header';
 import StudyList from 'components/studyList/studyList/StudyList';
 import SearchBox from 'components/studyList/searchBox/SearchBox';
+import LoadingPage from 'common/LoadingPage';
 
 import { Container } from 'style/CustomStyle';
 
 function StudyListContainer() {
-  const { searchToggle, studyList } = useSelector((state) => state.studyList);
-  console.log(studyList);
+  const {
+    searchToggle,
+    studyList: { loading, data, error },
+  } = useSelector((state) => state.studyList);
+  console.log(data);
   const dispatch = useDispatch();
 
   const onToggle = () => dispatch(toggleSelect());
@@ -22,6 +26,11 @@ function StudyListContainer() {
 
     dispatch(fetchStudyList());
   }, [dispatch]);
+
+  if (loading) return <LoadingPage />;
+  if (error) return <h1 style={{ fontSize: '50px' }}>{`에러가 발생했습니다!${error}`}</h1>;
+  if (!data) return null;
+
   return (
     <>
       <Header />
